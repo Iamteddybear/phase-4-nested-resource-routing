96bea71f37afba0986db1dddfb89cf90aaa1f546
ned <nedschultz@outlook.com>
2023-07-05 14:51:38 +1000

    finished

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -26,4 +26,14 @@ class ReviewsController < ApplicationController
     params.permit(:username, :comment, :rating)
   end
 
+  def index
+    if params[:dog_house_id]
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews
+    else
+      reviews = Review.all
+    end
+    render json: reviews, include: :dog_house
+  end
+
 end
